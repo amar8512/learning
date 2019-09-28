be77a1b6d6acea56cc4807db982202406bed57dd
Amarnath Singh <amarsing@adobe.com>
2019-09-28 16:24:43 +0530

    fixing untracked changes

--- src/com/noida/learning/SynchronizedVersion.java
+++ src/com/noida/learning/SynchronizedVersion.java
@@ -12,6 +12,7 @@ import java.util.*;
 public class SynchronizedVersion {
  
 	public static void main(String args[]){
+		// i am again adding some changes to check it with remote ...
 		// i am just adding a change to check if the git works properly or not.
 		List l1 = new ArrayList();
 		
