4bcbe863ffdf9470a4bfa3b5a58e530821b00d9c
Amar Singh <amarsing@adobe.com>
2020-07-10 12:23:04 +0530

    Changes in Two Sum sorted.

---
+++ src/com/noida/learning/ds/TwoSumUnsorted.java
@@ -0,0 +1,18 @@
+package com.noida.learning.ds;
+
+import java.util.HashMap;
+
+public class TwoSumUnsorted {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hm = new HashMap();
+        int indices[] = new int[2];
+     //   for
+        return new int[]{-1,-1};
+    }
+
+    public static void main(String[] args) {
+        int input[] = new int[]{3,3};
+      twoSum(input,6);
+    }
+
+}
