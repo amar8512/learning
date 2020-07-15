13891ee4caa9a0f66db6ddabb261577e2970b862
Amar Singh <amarsing@adobe.com>
2020-07-16 01:24:37 +0530

    moving zeros to begining.

---
+++ src/com/noida/learning/ds/MoveZeroBegining.java
@@ -0,0 +1,30 @@
+package com.noida.learning.ds;
+
+import com.noida.learning.java.Utilities;
+
+public class MoveZeroBegining {
+
+    public static int[] moveZeroes(int[] nums) {
+        int writerIndex = 0;
+        int readerIndex=0;
+       while(readerIndex< nums.length) {
+           if (nums[readerIndex] != 0) {
+               nums[writerIndex] = nums[readerIndex];
+               ++writerIndex;
+           }
+          ++readerIndex;
+       }
+       while(writerIndex<nums.length){
+           nums[writerIndex] =0;
+           writerIndex++;
+       }
+       return  nums;
+       }
+
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{0,1,0,3,12};
+       arr = moveZeroes(arr);
+        Utilities.print(arr);
+    }
+}
