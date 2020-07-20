553e986876b9c84679e527b7cff8bfbd7fbce5f5
Amar Singh <amarsing@adobe.com>
2020-07-21 01:49:44 +0530

    Longest Substring with k distinct characters : leetcode problem

---
+++ src/com/noida/learning/LongestSubStringWithMUnique.java
@@ -0,0 +1,33 @@
+package com.noida.learning;
+
+import java.util.HashMap;
+
+public class LongestSubStringWithMUnique {
+    public static int findLength(String str, int k) {
+        HashMap<Character,Integer> countOfChars = new HashMap<>();
+        int left=0 ,right=0,max=0;
+        for(right=0;right<str.length();right++){
+            if(countOfChars.containsKey(str.charAt(right))){
+                countOfChars.put(str.charAt(right),countOfChars.get(str.charAt(right))+1);
+            }
+            else{
+                countOfChars.put(str.charAt(right),1);
+
+            }
+            while(countOfChars.size()>k){
+                countOfChars.put(str.charAt(left),countOfChars.get(str.charAt(left))-1);
+                if(countOfChars.get(str.charAt(left)) ==0 ){
+                    countOfChars.remove(str.charAt(left));
+                }
+                ++left;
+            }
+            max = Math.max(max,right-left+1);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findLength("araaci", 2));
+    }
+}
