cd1f6ab88543f3d2706743c75ee6c89bca007698
Amar Singh <amarsing@adobe.com>
2020-08-17 02:09:15 +0530

    Priority Queue as Heap

---
+++ src/com/noida/learning/ds/PriorityQueueDemo.java
@@ -0,0 +1,32 @@
+package com.noida.learning.ds;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+    public static PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>(
+            new Comparator<Integer>() {
+                @Override public int compare(Integer o1, Integer o2) {
+                    return -1*o1.compareTo(o2);
+                }
+            });
+    public static  void add(int num){
+        priorityQueue.add(num);
+        System.out.println(priorityQueue.peek());
+    }
+    public static  void display(){
+        System.out.println(priorityQueue.remove());
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[]{10,12,0,2,-1,22,3,71,1};
+        for(int i =0;i<arr.length;i++){
+            add(arr[i]);
+        }
+        System.out.println("poll "+priorityQueue.poll());
+        System.out.println("poll "+priorityQueue.poll());
+        System.out.println("poll "+priorityQueue.poll());
+        System.out.println("peek "+priorityQueue.peek());
+        System.out.println("peek "+priorityQueue.peek());
+    }
+
+}
