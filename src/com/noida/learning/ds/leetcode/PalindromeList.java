23447bf9954dbfa2de449254fb8b135324be5ca7
Amar Singh <amarsing@adobe.com>
2020-08-31 10:43:52 +0530

    Check if a linked list is palindorme or not.

---
+++ src/com/noida/learning/ds/leetcode/PalindromeList.java
@@ -0,0 +1,60 @@
+package  com.noida.learning.ds.leetcode;
+class ListNode {
+	int value = 0;
+	ListNode next;
+
+	ListNode(int value) {
+		this.value = value;
+	}
+}
+
+class PalindromicLinkedList {
+
+	public static ListNode reverseLinkedList(ListNode head){
+		ListNode previous = null;
+		while(head!=null){
+			ListNode next = head.next;
+			head.next = previous;
+			previous = head;
+			head =next;
+		}
+		return  previous;
+	}
+	public static boolean isPalindrome(ListNode head) {
+		if(head==null|| head.next ==null){
+			return false;
+		}
+		ListNode fast = head;
+		ListNode slow = head;
+		ListNode start = head;
+		while(fast!=null && fast.next!=null){
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		ListNode middleNode = reverseLinkedList(slow);
+		ListNode copySecondHalf= middleNode;
+		while(head!=null && middleNode!=null){
+			if(head.value!=middleNode.value){
+				break;
+			}
+			head=head.next;
+			middleNode =middleNode.next;
+		}
+        reverseLinkedList(copySecondHalf);
+		if(head==null||copySecondHalf ==null)
+			return true;
+		return false;
+	}
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode(2);
+		head.next = new ListNode(4);
+		head.next.next = new ListNode(6);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(2);
+		System.out.println("Is palindrome: " + PalindromicLinkedList.isPalindrome(head));
+
+		head.next.next.next.next.next = new ListNode(2);
+		System.out.println("Is palindrome: " + PalindromicLinkedList.isPalindrome(head));
+	}
+}
