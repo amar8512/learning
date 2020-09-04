d8b3bf5c9b4b3025d9745a68a6e47f9f250b72d7
Amar Singh <amarsing@adobe.com>
2020-09-04 10:08:41 +0530

    Finding missing number using cyclic sort
    Finding missing number usiing cyclic sort , given a list of numbers from 0 to n, but our array contains only n-1 number , we have to find the missing number

---
+++ src/com/noida/learning/ds/leetcode/CyclicSortMissingNumber.java
@@ -0,0 +1,36 @@
+package com.noida.learning.ds.leetcode;
+
+import com.noida.learning.ds.QuickSort;
+
+public class CyclicSortMissingNumber {
+	public static void swapArray(int arr[] ,int index1 ,int index2){
+		int temp= arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+
+	public  static int cyclicSortAndFindMissingNUmber(int arr[]){
+		if(arr==null || arr.length==1){
+			return -1;
+		}
+		for(int i=0;i<arr.length;){
+			while (arr[i]<arr.length && arr[i]!=i){
+				QuickSort.swapArray(arr,arr[i],i);
+			}
+			i++;
+		}
+       for(int i=0;i<arr.length;i++){
+       	if(arr[i]!=i){
+       		return i;
+        }
+       }
+
+		return  arr.length;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = new int[]{9,6,4,2,3,5,7,0,1};
+		int misingNumber = cyclicSortAndFindMissingNUmber(arr);
+		System.out.println(""+misingNumber);
+	}
+}
