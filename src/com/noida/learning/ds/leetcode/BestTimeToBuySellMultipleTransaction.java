293d34c2723cca2cae5518d75f6947f0e6996825
Amar Singh <amarsing@adobe.com>
2020-12-04 00:11:13 +0530

    Adding some comments

--- src/com/noida/learning/ds/leetcode/BestTimeToBuySellMultipleTransaction.java
+++ src/com/noida/learning/ds/leetcode/BestTimeToBuySellMultipleTransaction.java
@@ -1,5 +1,13 @@
 package com.noida.learning.ds.leetcode;
 
+/**
+ *  This is the variation of the question to find the best time to sell a stock
+ *  However here is a small twist we have to find the total profit earned by doing multiple transations
+ *  from day 1 to day n.
+ *  The basic logic is find all those elements which are greater than there previous element and
+ *  store the result in max Profit variable.
+ *  The solution here uses greedy programming approach maxPeek Valley approach.
+ */
 public class BestTimeToBuySellMultipleTransaction {
     static int maxProfit(int[] prices) {
         int maxProfilt = 0;
